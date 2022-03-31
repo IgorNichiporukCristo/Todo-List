dc1959fb42ac3151c13e1644a19b01672eb49757
igor <inichiporuk1992@gmail.com>
2022-03-31 12:40:53 +0300

    Updates

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import addImage from './reducers/addImage'
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(addImage, composeEnhancer(applyMiddleware(thunk)));
-
-export default store;
